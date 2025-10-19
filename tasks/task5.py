c283c6c5270aba5901190c09ae04eb356ff10eed
Alakin-Aleksandr <alakinsasa2008@gmail.com>
2025-10-19 16:29:11 +0500

    Сделал дз

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,8 +2,19 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-   
+    a, b, c = map(str, input().split())
+    sim1 = a
+    sim2 = b
+    sim3 = c
+    code1 = ord(a)
+    code2 = ord(b)
+    code3 = ord(c)
+    outut1 = "Код символа " + str(sim1) + " Равен " + str(code1)
+    output2 = "Код символа " + str(sim2) + " Равен " + str(code2)
+    output3 = "Код символа " + str(sim3) + " Равен " + str(code3)
+    print(outut1)
+    print(output2)
+    print(output3)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
