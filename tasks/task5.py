345bd0c20b0cb32f012dd3ea6c087a9880228d48
Alakin-Aleksandr <alakinsasa2008@gmail.com>
2025-10-19 16:43:49 +0500

    Переделал

--- tasks/task5.py
+++ tasks/task5.py
@@ -9,7 +9,7 @@ def solve():
     code1 = ord(a)
     code2 = ord(b)
     code3 = ord(c)
-    print("Код символа " + str(sim1) + " Равен " + str(code1) + "\n" + "Код символа " + str(sim2) + " Равен " + str(code2) + "\n" + "Код символа " + str(sim3) + " Равен " + str(code3))
+    print("Код символа " + str(sim1) + " равен " + str(code1) + "\n" + "Код символа " + str(sim2) + " равен " + str(code2) + "\n" + "Код символа " + str(sim3) + " равен " + str(code3))
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
